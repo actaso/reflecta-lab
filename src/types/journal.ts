d74da89ff26fd9aad0c802e63cf3eb3de487ef05
louismorgner <louis@louismorgner.com>
2025-08-04 11:07:05 +0200

    improved user doc type

--- src/types/journal.ts
+++ src/types/journal.ts
@@ -17,11 +17,24 @@ export type ImageMetadata = {
   uploadedAt: Date; // when image was uploaded
 };
 
-
-
 // collection name on firestore: "users"
 export type UserAccount = {
   uid: string;
   createdAt: Date;
   updatedAt: Date;
+  firstName: string;
+  onboardingAnswers: {
+      onboardingCompleted: boolean;
+      onboardingCompletedAt: number; // unix timestamp
+      whatDoYouDoInLife: string[]; // string of tags selected
+      selfReflectionPracticesTried: string[]; // string of tags selected
+      clarityInLife: number; // 0 being totally unclear, 10 being very clear
+      stressInLife: number; // 0 being totally not stressed, 10 being very stressed
+  }
+  coachingConfig: {
+      challengeDegree: 'gentle' | 'moderate' | 'challenging' | 'intense';
+      harshToneDegree: 'supportive' | 'direct' | 'firm' | 'harsh';
+      coachingMessageFrequency: 'daily' | 'multipleTimesPerWeek' | 'onceAWeek';
+      enableCoachingMessages: boolean; // if true, based on frequency messages will be sent. this should be a setting in the user doc.
+  }
 };
