3cc58ad35c6d2c7b8ec1a8ae78f5052b0ceca300
Invalid Luca <55715525+LujoTheRealOne@users.noreply.github.com>
2025-08-11 17:19:40 +0200

    removed unused but defined import

--- src/services/pushNotificationService.ts
+++ src/services/pushNotificationService.ts
@@ -1,4 +1,4 @@
-import { Expo, ExpoPushMessage, ExpoPushTicket, ExpoPushReceipt } from 'expo-server-sdk';
+import { Expo, ExpoPushMessage, ExpoPushTicket } from 'expo-server-sdk';
 import { FirestoreAdminService } from './firestoreAdminService';
 import { 
   PushNotificationResult, 
